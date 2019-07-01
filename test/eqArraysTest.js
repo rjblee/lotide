756cc3a4f7fabe95858bc34594dff65b030f060d
Ralph <ralph.lee@live.ca>
2019-07-01 20:54:46 +0000

    Added eqArraysTest

---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
