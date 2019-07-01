b8c305a478ee18fb4441cb7d6d62ff601e98253c
Ralph <ralph.lee@live.ca>
2019-07-01 23:35:49 +0000

    Added Mocha and Chai to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,13 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns ['5'] for ['5']", () => {
+    assert.deepEqual(middle(['5']), ['5']); 
+  });
+});
