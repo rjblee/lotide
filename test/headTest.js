7b3a22be0e3c53d151f5b9c9f15e4a55168c7f3e
Ralph <ralph.lee@live.ca>
2019-07-01 22:10:33 +0000

    Added Mocha and Chai to headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
 
-  // TEST CODE
-  assertEqual("Bootcamps", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual(head([5,6,7]), 5);
+//   // TEST CODE
+//   assertEqual("Bootcamps", "Bootcamp");
+//   assertEqual(1, 1);
+//   assertEqual(head([5,6,7]), 5);
+
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
