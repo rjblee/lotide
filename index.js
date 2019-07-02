8b1a977ee592eeee73abe6e240a5c74c10b2814c
Ralph <ralph.lee@live.ca>
2019-07-02 01:08:24 +0000

    Added Object Shorthand for all functions

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 const middle = require('./middle');
+const tail   = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
